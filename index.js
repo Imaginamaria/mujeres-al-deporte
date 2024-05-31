b60b407872c31b2594f98300566942ac359f3174
imaginamaria <emaria@imagina.com.uy>
2024-05-30 22:18:38 -0300

    agregue lo refernte a voleibol

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ import 'bs5-lightbox';
 
 import $ from 'jquery';
 
+
+var btnAbrirModal = document.querySelector("#btnAbrirModal");
+var articuloModal = document.querySelector("#articuloModal");
+
+btnAbrirModal.addEventListener("click", function() {
+  var modal = new bootstrap.Modal(articuloModal);
+  modal.show();
+});
+
